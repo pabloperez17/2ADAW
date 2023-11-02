4cb70b17a4b80901a9aa360547f7e1b5b657baef
Pablo Pérez Iza <perez.iza.pablo@gmail.com>
2023-11-02 12:46:41 +0100

    02/11/2023

--- JS/2ª relacion/Ej17/Ej17.js
+++ JS/2ª relacion/Ej17/Ej17.js
@@ -1,13 +1,67 @@
-document.querySelectorAll("button")[0].addEventListener("click", function () {
+let intervalo;
 
+document.querySelectorAll("button")[0].addEventListener("click", function () {
+  this.disabled = true;
+  intervalo = setInterval(correr, 200);
+
+  function correr() {
+    posiciones[0] += random(0, 50);
+    posiciones[1] += random(0, 50);
+    posiciones[2] += random(0, 50);
+    posiciones[3] += random(0, 50);
+    document
+      .querySelectorAll("div")[0]
+      .style.setProperty("transform", `translateX(${posiciones[0]}px)`);
+    document
+      .querySelectorAll("div")[1]
+      .style.setProperty("transform", `translateX(${posiciones[1]}px)`);
+    document
+      .querySelectorAll("div")[2]
+      .style.setProperty("transform", `translateX(${posiciones[2]}px)`);
+    document
+      .querySelectorAll("div")[3]
+      .style.setProperty("transform", `translateX(${posiciones[3]}px)`);
+
+    if (posiciones[0] >= window.innerWidth - 100) {
+      posiciones[0] = window.innerWidth - 100;
+      alert("Ha ganado el verde");
+      clearInterval(intervalo);
+    } else if (posiciones[1] >= window.innerWidth - 100) {
+      posiciones[1] = window.innerWidth - 100;
+      alert("Ha ganado el rojo");
+      clearInterval(intervalo);
+    } else if (posiciones[2] >= window.innerWidth - 100) {
+      posiciones[2] = window.innerWidth - 100;
+      alert("Ha ganado el azul");
+      clearInterval(intervalo);
+    } else if (posiciones[3] >= window.innerWidth - 100) {
+      posiciones[3] = window.innerWidth - 100;
+      alert("Ha ganado el amarillo");
+      clearInterval(intervalo);
+    }
+  }
 });
+let posiciones = [0, 0, 0, 0];
 
 document.querySelectorAll("button")[1].addEventListener("click", function () {
+  document.querySelectorAll("button")[0].disabled=false;
+  posiciones = [0, 0, 0, 0];
+  document
+    .querySelectorAll("div")[0]
+    .style.setProperty("transform", `translateX(${posiciones[0]}px)`);
+  document
+    .querySelectorAll("div")[1]
+    .style.setProperty("transform", `translateX(${posiciones[1]}px)`);
+  document
+    .querySelectorAll("div")[2]
+    .style.setProperty("transform", `translateX(${posiciones[2]}px)`);
+  document
+    .querySelectorAll("div")[3]
+    .style.setProperty("transform", `translateX(${posiciones[3]}px)`);
 
-
+  clearInterval(intervalo);
 });
 
-function correr() {
-//mover divs a la derecha aleatoriamente
-    document.querySelectorAll("div").style.left = Math.floor(Math.random() * 100) + "%";
+function random(min, max) {
+  return Math.random() * (max - min) + min;
 }
