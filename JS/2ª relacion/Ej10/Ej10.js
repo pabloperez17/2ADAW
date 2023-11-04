9d9608a4a152e8fd0f926cf29fbfb389bfbc9542
Pablo Pérez Iza <perez.iza.pablo@gmail.com>
2023-11-04 13:56:29 +0100

    ejercicio 10 js

--- JS/2ª relacion/Ej10/Ej10.js
+++ JS/2ª relacion/Ej10/Ej10.js
@@ -1,3 +1,14 @@
-function cambiarfondo(){
-    addEventListener("mouseover", (event) => {});
+const divs = document.querySelectorAll("div");
+console.log(divs);
+for (let div of divs){
+    div.addEventListener("mouseover", function(){
+        cambiarfondo(window.getComputedStyle(div).getPropertyValue("background-color"));
+    });
+    div.addEventListener("mouseleave",function(){
+        document.getElementsByTagName("body")[0].style.setProperty("background-color","white");
+    });
+}
+
+function cambiarfondo(color){
+    document.getElementsByTagName("body")[0].style.setProperty("background-color", color);
 }
