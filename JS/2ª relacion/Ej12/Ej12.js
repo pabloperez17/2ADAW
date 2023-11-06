b18ea75ca2b3fea2a2d57c504ffa5c8c85c43331
Pablo Pérez Iza <perez.iza.pablo@gmail.com>
2023-11-07 00:41:07 +0100

    mejoras

--- JS/2ª relacion/Ej12/Ej12.js
+++ JS/2ª relacion/Ej12/Ej12.js
@@ -2,15 +2,13 @@ let div = document.getElementsByTagName("div")[0];
 
 document.getElementsByTagName("input")[0].addEventListener("click",function(){
     let posicion_izquierda = window.getComputedStyle(div).left;
-    let valor = parseInt(posicion_izquierda) + 50;
-
+    let valor = parseInt(posicion_izquierda) + 5;
     if (valor + div.offsetWidth <= window.innerWidth ){
         div.style.left = valor + "px";
     } else {
         div.style.left = window.innerWidth - div.offsetWidth - 5;
         document.getElementsByTagName("input")[0].disabled = true
     } 
-
 });
 
 document.getElementsByTagName("input")[1].addEventListener("click",function(){
