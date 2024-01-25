e912dbdc2d360842211c5d50aa055fdb32639a19
Pablo Pérez Iza <perez.iza.pablo@gmail.com>
2024-01-25 14:35:47 +0100

    25/01/2024

--- Interfaces/MaquetacionWebCV - para alumnos - Práctica/js/script.js
+++ Interfaces/MaquetacionWebCV - para alumnos - Práctica/js/script.js
@@ -11,15 +11,17 @@ $(document).ready(() => {
         if ($(':root').css('--color-principal') == '#FFFFFF') {
             $(':root').css('--color-principal', '#191970');
             $(':root').css('--color-secondary', '#d0e6ff');
+            //$(':root').css('----color-background', '#00008b'); No me funciona 
             $(':root').css('--color-title', '#FFFFFF');
             $(':root').css('--color-subtitle', '#FFFFFF');
             $(':root').css('--color-text', '#FFFFFF');
             $('.oscuro').addClass('fa-regular fa-sun').removeClass('fa-solid fa-moon');
             $('#cambiarSpan').text('Modo Claro');
-            // claro
         } else {
+        // claro
             $(':root').css('--color-principal', '#FFFFFF');
             $(':root').css('--color-secondary', '#53ceff');
+            //$(':root').css('----color-background', '#F2F7F9');
             $(':root').css('--color-title', '#222222');
             $(':root').css('--color-subtitle', '#888');
             $(':root').css('--color-text', '#555');
@@ -42,5 +44,17 @@ $(document).ready(() => {
         }, 800);
         return false;
     });
+
+    //Menu hamburguesa
+    (function () {
+        $('.user-info_general').on('click', function() {
+            $('.user-info__container-image').toggleClass('animate');
+            $('.layout_menu').toggleClass('visible');
+        })
+        $('.layout_menu > ul > li > a').on('click', function () {
+            $('.user-info__container-image').removeClass('animate');
+            $('.layout_menu').removeClass('visible');
+        })
+    })();
 
 });
